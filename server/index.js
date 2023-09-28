8d8170f81db09fa25f3e236497b8e5a4f493a866
Angga Saputra <angsa0077@gmail.com>
2023-09-29 03:47:18 +0700

    connect to database mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 import products from "./data/products.js";
 
 const app = express();
@@ -19,4 +20,13 @@ app.get("/api/products", (req, res) => {
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
+})
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("DB connection successful");
+}).catch((err) => {
+    console.log("DB connection failed", err.message);
 })
